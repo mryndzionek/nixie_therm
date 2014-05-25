8f5baeb6dfe628a87ee08032a650d77affdaad88
mryndzionek <mryndzionek@gmail.com>
2014-05-25 16:49:42 +0200

    Added dimming support

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 #define LED PD6
 #define PWM_DIGIT_1 PB2
 #define PWM_DIGIT_2 PB3
+#define BUTTON PB0
+
 #define PCF8574_ADDRESS 0x38
 
 #define output_low(port,pin) port &= ~(1<<pin)
@@ -12,18 +14,42 @@
 #define set_input(portdir,pin) portdir &= ~(1<<pin)
 #define set_output(portdir,pin) portdir |= (1<<pin)
 
+static const unsigned char DIM_SCALE[] = { 0x05, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11,
+        0x12, 0x13, 0x20, 0x25, 0x35, 0x40, 0x45, 0x50,
+        0x55, 0x60, 0x65, 0x70, 0x75, 0x80, 0x90, 0xa0,
+        0xb0, 0xc0, 0xd0, 0xe0, 0xf0, 0xff, 0xff, 0xff
+};
+
 static void setup() {
+
     set_output(DDRD, LED);
     set_output(DDRB, PWM_DIGIT_1);
+    TCCR0A = _BV(COM0A1) | _BV(WGM00);
+    OCR0A  = 0xFF;
+    TCCR0B = _BV(CS01) | _BV(CS00);
+
     set_output(DDRB, PWM_DIGIT_2);
-    output_high(PORTB, PWM_DIGIT_1);
-    output_high(PORTB, PWM_DIGIT_2);
+    TCCR1A = (1 << COM1A1)| (1 << WGM11) | (1 << WGM10);
+    OCR1A = 0x3FF;
+    TCCR1B = _BV(CS10) | _BV(CS11);
+
+
+    set_input(DDRB, BUTTON);
 
     USI_TWI_Master_Initialise();
 }
 
 static void dimm_digit(unsigned int digit, unsigned int level)
 {
+    if(level > 0xFF) level = 0xFF;
+
+    unsigned char scaled_level = DIM_SCALE[level >> 3];
+
+    if(digit == 0)
+        OCR0A = scaled_level;
+    else {
+        OCR1A = scaled_level << 2;
+    }
 
 }
 
@@ -53,11 +79,18 @@ static unsigned int display_number(unsigned int n) {
 int main(void) {
 
     int n = 0;
+    unsigned int level = 0;
 
     setup();
 
     while (1) {
             display_number(n++);
+
+            dimm_digit(0, level);
+            dimm_digit(1, level);
+            level++;
+            if(level > 0xFF)
+                level = 0;
             if(n > 99)
                 n = 0;
             _delay_ms(100);
