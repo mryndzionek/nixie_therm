07c32b98f86c86ce6fd94d83453da7879ecfa425
mryndzionek <mryndzionek@gmail.com>
2014-05-25 15:26:59 +0200

    Added USI TWI support

--- src/main.c
+++ src/main.c
@@ -1,36 +1,53 @@
-#define F_CPU                8000000UL
-
+#include "USI_TWI_Master.h"
 #include <avr/io.h>
+#include <util/delay.h>
 
 #define LED PD6
+#define PWM_DIGIT_1 PB2
+#define PWM_DIGIT_2 PB3
+#define PCF8574_ADDRESS 0x38
 
 #define output_low(port,pin) port &= ~(1<<pin)
 #define output_high(port,pin) port |= (1<<pin)
 #define set_input(portdir,pin) portdir &= ~(1<<pin)
 #define set_output(portdir,pin) portdir |= (1<<pin)
 
-// this is just a program that 'kills time' in a calibrated method
-void delay_ms(uint8_t ms) {
-	uint16_t delay_count = F_CPU / 17500;
-	volatile uint16_t i;
+static void setup() {
+    set_output(DDRD, LED);
+    set_output(DDRB, PWM_DIGIT_1);
+    set_output(DDRB, PWM_DIGIT_2);
+    output_high(PORTB, PWM_DIGIT_1);
+    output_high(PORTB, PWM_DIGIT_2);
 
-	while (ms != 0) {
-		for (i=0; i != delay_count; i++);
-		ms--;
+    USI_TWI_Master_Initialise();
 }
+
+static void blink(int n)
+{
+    int i;
+    for(i=0; i<n;i++)
+        {
+            output_high(PORTD, LED);
+            _delay_ms(100);
+            // now turn off the LED for another 200ms
+            output_low(PORTD, LED);
+            _delay_ms(200);
+        }
 }
 
 int main(void) {
-// initialize the direction of PORTD #6 to be an output
-set_output(DDRD, LED);  
 
-	while (1) {
-		// turn on the LED for 200ms
-		output_high(PORTD, LED);
-		delay_ms(200);
-		// now turn off the LED for another 200ms
-		output_low(PORTD, LED);
-		delay_ms(200);
-		// now start over
-	}
+    unsigned char data[2];
+
+    setup();
+
+    data[0] = PCF8574_ADDRESS << 1;
+    data[1] = 0x11;
+    USI_TWI_Start_Read_Write(data, 2);
+    int s = USI_TWI_Get_State_Info();
+
+    while (1) {
+            blink(s);
+            _delay_ms(1000);
+    }
 }
